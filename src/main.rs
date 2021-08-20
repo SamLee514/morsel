de00d17975b82356b6b436bcba6d6193124dd533
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-08-20 00:33:55 -0400

    input set up

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use std::{
     time::{Duration, Instant},
 };
 use structopt::StructOpt;
-use termion::clear;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::{self, async_stdin, event::Key};
+use termion::{clear, cursor};
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "morsel", about = "morse code CLI tool")]
@@ -37,19 +37,30 @@ fn main() {
     }
     write!(io::stdout().into_raw_mode().unwrap(), "\n").unwrap();
     let mut stdin = async_stdin().keys();
-    let mut writer =
-        writer::Writer::new(async_stdin().keys(), io::stdout().into_raw_mode().unwrap());
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+    // let mut writer =
+    //     writer::Writer::new(async_stdin().keys(), io::stdout().into_raw_mode().unwrap());
 
     let mut now;
     let mut was_keydown = false;
     let mut start = Instant::now();
+    let mut count = 0;
     loop {
         let inp = stdin.next();
-        // Print output by the millisecond
         now = start.elapsed();
+
         if let Some(Ok(c)) = inp {
+            write!(stdout, "Hey!");
+            write!(stdout, "{}", cursor::Left(3)).unwrap();
+            write!(stdout, "{}", clear::AfterCursor).unwrap();
+            io::stdout().flush().unwrap();
+            // count += 1;
             match c {
-                Key::Char('q') => return,
+                Key::Char('q') => {
+                    writeln!(stdout, "{}", count);
+                    // io::stdout().flush().unwrap();
+                    return;
+                }
                 _ => {
                     if !was_keydown {
                         was_keydown = true;
@@ -57,32 +68,34 @@ fn main() {
                     }
                 }
             };
-        } else {
-            // Key up events. Either end a key down or start putting in spaces.
-            if was_keydown {
-                was_keydown = false;
-                // Enter a letter
-                if start.elapsed() < DAH_LEN {
-                    // Fire off a dit
-                    writer.process_input(writer::tree::Input::Dit);
-                } else {
-                    // Fire off a dah
-                    writer.process_input(writer::tree::Input::Dah);
-                }
-                start = Instant::now();
-            } else {
-                // TODO: possibly direct equality will skip
-                if start.elapsed() == LONG_DAH_LEN {
-                    // Fire off a space
-                    start = Instant::now();
-                } else if start.elapsed() == DAH_LEN {
-                    // Fire off a letter check. If the letter failed, reset the timer so we don't fire a space too early.
-                    match writer.process_input(writer::tree::Input::Space) {
-                        Ok(_) => {}
-                        Err(e) => start = Instant::now(),
-                    }
-                }
-            }
-        }
+        } //else {
+          //     // write!(io::stdout().into_raw_mode().unwrap(), "Bye!");
+          //     // io::stdout().flush().unwrap();
+          //     // Key up events. Either end a key down or start putting in spaces.
+          //     if was_keydown {
+          //         was_keydown = false;
+          //         // Enter a letter
+          //         if start.elapsed() < DAH_LEN {
+          //             // Fire off a dit
+          //             // writer.process_input(writer::tree::Input::Dit);
+          //         } else {
+          //             // Fire off a dah
+          //             // writer.process_input(writer::tree::Input::Dah);
+          //         }
+          //         start = Instant::now();
+          //     } else {
+          //         // TODO: possibly direct equality will skip
+          //         if start.elapsed() == LONG_DAH_LEN {
+          //             // Fire off a space
+          //             start = Instant::now();
+          //         } else if start.elapsed() == DAH_LEN {
+          //             // Fire off a letter check. If the letter failed, reset the timer so we don't fire a space too early.
+          //             // match writer.process_input(writer::tree::Input::Space) {
+          //             //     Ok(_) => {}
+          //             //     Err(e) => start = Instant::now(),
+          //             // }
+          //         }
+          //     }
+          // }
     }
 }
